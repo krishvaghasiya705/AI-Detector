c36262e9facdc9af1d4c79d62b1c14a359e0ea66
krish vaghasiya <kvaghasiya705@gmail.com>
2025-04-28 12:45:09 +0530

    fix: Replace Head component with standard head tag in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import '@/styles/global.scss';
 import Header from './common/header';
 import Footer from './common/footer';
@@ -6,12 +5,12 @@ import Footer from './common/footer';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <meta name="description" content="This is a test application." />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet"/>
         <title>AI Detector | Detect AI Content in Seconds</title>
-      </Head>
+      </head>
       <body>
         <Header />
         <main>{children}</main>
